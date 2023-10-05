9a578403695cf92ef12bc4decb3c06b5304e3f0f
nakamarcio <122229176+nakamarcio@users.noreply.github.com>
2023-10-05 10:30:37 -0300

    Delete codoficial-main/frontend directory

--- codoficial-main/frontend/src/components/OnlyForSuperUser/index.js
+++
@@ -1,9 +0,0 @@
-const OnlyForSuperUser = ({ user, yes, no }) => user.superAdmin ? yes() : no();
-
-OnlyForSuperUser.defaultProps = {
-    user: {},
-	yes: () => null,
-	no: () => null,
-};
-
-export default OnlyForSuperUser;
